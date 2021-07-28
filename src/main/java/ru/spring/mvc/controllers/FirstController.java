ffad75ccd9fd6b3fd4b01b24a5fe3f95962f743c
SmokedKoala <k.azarov@inbox.ru>
2021-07-28 15:29:27 +0300

    Controller
    реализованы 2 простейших контроллера

---
+++ src/main/java/ru/spring/mvc/controllers/FirstController.java
@@ -0,0 +1,5 @@
+package ru.spring.mvc.controllers;/**
+ * todo Document type FirstController
+ */
+public class FirstController {
+}
---
+++ src/main/java/ru/spring/mvc/controllers/SecondController.java
@@ -0,0 +1,5 @@
+package ru.spring.mvc.controllers;/**
+ * todo Document type SecondController
+ */
+public class SecondController {
+}
