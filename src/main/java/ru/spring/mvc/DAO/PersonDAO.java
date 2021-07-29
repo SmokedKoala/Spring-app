638e46b55e7cdc200a982e9b2fb4d7662ba32988
SmokedKoala <k.azarov@inbox.ru>
2021-07-29 12:31:21 +0300

    DELETE, PATCH
    реализованы все запросы для работы с данными

--- src/main/java/ru/spring/mvc/DAO/PersonDAO.java
+++ src/main/java/ru/spring/mvc/DAO/PersonDAO.java
@@ -35,6 +35,15 @@ public class PersonDAO {
         people.add(person);
     }
 
+    public void update(int id, Person updatePerson){
+        Person person = showOne(id);
+
+        person.setName(updatePerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
+    }
 }
 
 
--- src/main/java/ru/spring/mvc/config/SpringMVCDespatcherServletInitializer.java
+++ src/main/java/ru/spring/mvc/config/SpringMVCDespatcherServletInitializer.java
@@ -1,11 +1,13 @@
 package ru.spring.mvc.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-/**
- * todo Document type SpringMVCDespatcherServletInitializer
- */
-public class SpringMVCDespatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+
+class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
@@ -13,11 +15,22 @@ public class SpringMVCDespatcherServletInitializer extends AbstractAnnotationCon
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {SpringConfig.class};
+        return new Class[]{SpringConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] {"/"};
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+            new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
